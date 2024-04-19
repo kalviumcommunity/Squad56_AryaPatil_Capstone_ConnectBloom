fb96f4ffa17d002ef6451f29e86f0b859f92575b
patilarya76 <patilaryaanil07@gmail.com>
2024-04-19 15:25:45 +0530

    codiumaichnagesdone

--- server/Model/user.js
+++ server/Model/user.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 // Define Mongoose schema
 const categoriesSchema = new mongoose.Schema({
-    img: { type: String, required: true },
+    image: { type: String, required: true },
     name: { type: String, required: true },
     benefits: { type: Array, required: true },
     category: { type: String, required: true },
 
 });
 
-const categoriesModel = mongoose.model("categories", categoriesSchema); 
+const categoriesModel = mongoose.model("Categories", categoriesSchema); 
 
 module.exports = { categoriesModel};
