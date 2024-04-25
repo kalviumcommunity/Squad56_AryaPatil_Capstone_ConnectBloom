81da2d32f255b44534b1a8a1adcb31c304b03c2a
patilarya76 <patilaryaanil07@gmail.com>
2024-04-25 10:38:57 +0530

    codiumaichanegsdone

--- server/Model/user.js
+++ server/Model/user.js
@@ -7,20 +7,13 @@ const categoriesSchema = new mongoose.Schema({
     benefits: { type: Array, required: true },
     category: { type: String, required: true },
     location:{type:String , required:true},
-    price:{type:String, required:true}
+    price:{type:Number, required:true}
 
 }); 
 
-const saleSchema = new mongoose.Schema({
-    image: { type: String, required: true },
-    name: { type: String, required: true },
-    benefits: { type: Array, required: true },
-    category: { type: String, required: true },
-    location: { type: String, required: true },
-    price: { type: String, required: true }
-});
+
 
 const categoriesModel = mongoose.model("categories", categoriesSchema);  
-const Sale = mongoose.model("sales", saleSchema);  
 
-module.exports = { categoriesModel,Sale };  
+
+module.exports = { categoriesModel };  
