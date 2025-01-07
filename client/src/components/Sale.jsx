224507a9b6083d04f7d37c7caeaf2298aacf6006
patilarya76 <patilaryaanil07@gmail.com>
2025-01-07 10:29:03 +0530

    donewith changes

--- client/src/components/Sale.jsx
+++ client/src/components/Sale.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from './Nav';
+import Navbar from './Nav.jsx';
 import "../components/Sales.css";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
